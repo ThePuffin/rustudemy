1b80afbd99eedfefa3b512715358c5158ca2eef9
VinzApside <vinzapisde@gmail.com>
2019-02-02 10:55:00 +0100

    middle part 4

---
+++ forth part/rust28.rs
@@ -0,0 +1,29 @@
+fn main () {
+    // add();
+    // anotherfunction(1,2);
+    // let res = otherfunction(1,8);
+    // println!("{}",res );
+    // //ou
+    // println!("{}",otherfunction(4,3));
+    
+println!("{:?}", sub_add(4, 2));
+
+
+}
+
+// fn add() {
+//     println!("{}", 2+3 )
+// }
+
+// fn anotherfunction(a:i32,b:i32){
+// println!("{} + {} = {}", a,b, a+b )
+// }
+
+// fn otherfunction(a:i32,b:i32)->i32{
+    // // return est optionnel
+//     return a+b
+// }
+
+fn sub_add(a:i32,b:i32)->(i32,i32){
+    (a+b,a-b)
+}
