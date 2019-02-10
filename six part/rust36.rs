2042e70453162278f12c75d0bfb087f8691ba6dc
VinzApside <vinzapisde@gmail.com>
2019-02-10 09:38:48 +0100

    memory owner

---
+++ six part/rust36.rs
@@ -0,0 +1,24 @@
+// fn main () {
+//     let a=10;
+// }
+
+// fn fun() {
+//     //'a' a déjà été attribué à un point de mémoire en dehors de la scope donc on a une erreur 
+//     a=20;
+// }
+
+// fn main () {
+//     let s= String::from("Hello");
+//     let s1=s;
+//     //provoque une erreur car la valeur a été déplacée
+//     // println!("{}", s );
+//     println!("{}", s1);
+// }
+
+fn main () {
+let s= String::from("Hello");
+//clone() permet de faire une copie sans réattribué le pointeur vers le point de mémoire
+let s1=s.clone();
+ println!("{}", s );
+ println!("{}", s1);
+}
