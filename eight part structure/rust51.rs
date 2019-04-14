2678372c37cd75e314aa7915b0cc7e757a632495
Vinz <ballutvincent@gmail.com>
2019-04-14 09:26:18 +0200

    multiple parameters

---
+++ eight part structure/rust51.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+//multiple value of parameters
+impl Rectangle {
+    fn can_hold(&self, other:&Rectangle)->bool{
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+
+let rect1=Rectangle {width: 30, height: 50};
+let rect2=Rectangle {width: 10, height: 30};
+let rect3=Rectangle {width : 60, height: 50};
+
+    println!("{}",rect1.can_hold(&rect2));
+    println!("{}",rect1.can_hold(&rect3));
+    println!("{}",rect3.can_hold(&rect2));
+
+}
