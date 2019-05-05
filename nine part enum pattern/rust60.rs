80279ac0e9592094a5233b14fa8062abc16b1a6a
Vinz <ballutvincent@gmail.com>
2019-05-05 11:12:49 +0200

    also can pass some value
    like a value

--- nine part enum pattern/rust60.rs
+++ nine part enum pattern/rust60.rs
@@ -5,7 +5,7 @@
 enum Coin {
     Penny,
     Nickel,
-    Dime,
+    Dime(i32),
     Quarter(UsState),
 }
 #[allow(dead_code)]
@@ -24,7 +24,10 @@ fn value_in_cents(c:Coin)->u32{
             1
         },
         Coin::Nickel=>5,
-        Coin::Dime=>10,
+        Coin::Dime(value)=>{
+            println!("{}", value);
+           10
+        },
         Coin::Quarter(state)=>{
             println!("{:?}", state);
             25
@@ -35,5 +38,6 @@ fn value_in_cents(c:Coin)->u32{
 
 fn main() {
  println!("{}", value_in_cents(Coin::Penny));   
- println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));   
+ println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));    
+ println!("{}", value_in_cents(Coin::Dime(666)));   
 }
