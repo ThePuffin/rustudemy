2e5a6f747e09d9976843e46be9fda9e48199d3cd
Vinz <ballutvincent@gmail.com>
2019-06-24 19:28:26 +0200

    end introduction to vector

--- eleven part/rust73.rs
+++ eleven part/rust73.rs
@@ -1,7 +1,11 @@
 fn main () {
-    let mut v=Vec::new();
+    let mut v:Vec<i32>=Vec::new();
     v.push(20);
     v.push(30); 
     v.push(40);
     println!("{:?}",v);
+
+       let vv=vec![1,2,3,4];
+    // rust auto detect type of values
+     println!("{:?}",vv);
 }
