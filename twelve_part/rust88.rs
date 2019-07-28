7fc3fd707005f9400927987c8eec5e3d2a20d7d0
Vinz <ballutvincent@gmail.com>
2019-07-28 10:55:05 +0200

    work that time

--- twelve_part/rust88.rs
+++ twelve_part/rust88.rs
@@ -1,20 +1,18 @@
 use::std::fs::File;
 use::std::io::ErrorKind;
-
 fn main () {
-    let f=File::open("hello.txt");
+    let f=File::open("hellos.txt");
     let f= match f {
             Ok(file)=>file,
             Err(ref error) if error.kind()==ErrorKind::NotFound=> { 
-                match File::create("hello.txt") {
+              match File::create("hellos.txt") {
                 Ok(fc)=>fc,
                 Err(e)=>{ 
                     panic!("Not able to create file {:?}", e)
                 },
             }
-        }
     },
-    Err(error)=> { panic!("Unable to open file {:?}",error);
-    },
+    Err(error)=> { panic!("Unable to open file {:?}",error);},
+    };
 
 }
