147a64dc3a63e338356f5ff935ab2476f333ac9a
Vinz <ballutvincent@gmail.com>
2019-07-28 09:32:06 +0200

    unrecoverable error with panic

---
+++ twelve_part/rust86.rs
@@ -0,0 +1,21 @@
+//Unrecoverable error
+
+fn main () {
+
+    // let a  = [1,2,3];
+    // a[99];
+    
+    // panic!("Checking how panic works");
+
+    let choice="buuy";
+
+    println!("What's your choice");
+    if choice == "buy"{
+        println!("Thank you");
+    }else {
+        panic!("Please buy it !");
+    };
+
+
+
+}
