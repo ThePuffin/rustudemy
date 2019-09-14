842282778345ee0e7b0f7fbd328c1f736e924846
Vinz <ballutvincent@gmail.com>
2019-09-14 10:35:26 +0200

    Assignment Solution Error Handling part 1

---
+++ twelve_part/rust92.rs
@@ -0,0 +1,47 @@
+//Unrecoverable error
+
+fn main () {
+    println!("Welcome");
+    let doc=document();
+    if doc==String::from("Yes"){
+        println!("Thank you for submitting documents");
+        println!("Now submit fees");
+        let fees=fees();
+        if fees==String::from("Yes"){
+            println!("Thank you for submitting fees, admission confirmed" );
+        }
+        else{
+              println!("Sorry you have not submitted fees, admission cancelled" );
+              panic!("");
+        }
+    } else {
+           println!("Sorry you have not submitted documents, admission cancelled" );
+              panic!("");
+    }
+}
+
+fn document () ->String {
+let doc="Yes";
+    println!("Do you have all documents");
+    if doc=="Yes" {
+        println!("{}\n", doc);
+        return String::from("Yes");
+    }
+    else {
+        println!("{}\n", doc);
+        return String::from("No");
+    }
+}
+
+fn fees () ->String {
+    let fees="No";
+    println!("Please submit your fees" );
+    if fees=="Ok" {
+        println!("{}\n", fees );
+        return String::from("Yes");
+    }
+    else{
+        println!("{}\n", fees );
+        return String::from("No");  
+    }
+}
